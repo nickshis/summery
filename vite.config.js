250530363e3b7aa7cdf1dd2ff507dee0301daf07
nickshis <pablopablovichh@gmail.com>
2024-04-22 23:02:27 +0500

    ss

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,9 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         england: resolve(__dirname, 'pages/england/index.html'),
         africa: resolve(__dirname, 'pages/africa/index.html'),
-        afandi: resolve(__dirname, 'pages/afandi/index.html')
+        afandi: resolve(__dirname, 'pages/afandi/index.html'),
+        diwali: resolve(__dirname, 'pages/diwali/index.html'),
+        teresa: resolve(__dirname, 'pages/teresa/index.html')
       },
     },
   },
